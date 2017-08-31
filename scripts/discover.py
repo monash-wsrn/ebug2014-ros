7186fe6ed2be12c2f1acde8d81c6fef9e50b0df9
Ahmet Sekercioglu <sekerci.ahmet@gmail.com>
2017-08-31 14:40:43 +0200

    Moved nrf.py to "libs".

--- scripts/discover.py
+++ scripts/discover.py
@@ -23,10 +23,10 @@ for addr, info in ebugs.items():
     nrf.LED_brightness(4)
     if camera_calibration == True:
         # Calibration LED pattern (upward looking set):
-        # D2 is illuminated white (0 degrees)
-        red   = 0x0800 + 0x1000 + 0x2000 + 0x4000 + 0x8000 + 0x0001 # LEDs  D4 to D10
+        # D2 is illuminated green (0 degrees)
+        red   = 0x0000 + 0x1000 + 0x2000 + 0x4000 + 0x8000 + 0x0001 # LEDs  D4 to D10
         green = 0x0800 + 0x0002 + 0x0004 + 0x0008 + 0x0010 + 0x0020 # LEDs D12 to D22
-        blue  = 0x0800 + 0x0040 + 0x0080 + 0x0100 + 0x0200 + 0x0400 # LEDs D24 to D32
+        blue  = 0x0000 + 0x0040 + 0x0080 + 0x0100 + 0x0200 + 0x0400 # LEDs D24 to D32
         nrf.set_LEDs(red, green, blue)
     else:
         red = info['led_sequence'][0]
