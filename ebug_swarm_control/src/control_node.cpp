6e3e78b4dded5e3c63fa99036fbb279dca7abbcf
Ahmet Sekercioglu <sekerci.ahmet@gmail.com>
2018-03-28 17:04:32 +0200

    Reorganizing the overall system. It will be more efficient to have a one package, and under it multiple nodes (instead of multiple packages).

--- ebug_swarm_control/src/control_node.cpp
+++ ebug_swarm_control/src/control_node.cpp
@@ -5,23 +5,36 @@ using namespace std;
 ros::Publisher *pubPtr;
 std::string position;
 
-void dataReceived(const ebug_swarm_msg::dataArray& arrayIn){
-
+void dataReceived(const ebug_swarm_msg::dataArray& arrayIn)
+{
   ROS_INFO_STREAM("Control node: Commands received and blobs sent.");
-  
 } // dataReceived()
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "controlnode");
-  
+  ros::init(argc, argv, "control");
   ros::NodeHandle nh;
-    
-  
-  pubPtr = new ros::Publisher(nh.advertise<ebug_swarm_msg::frame>("blobs",1000));    
-  
-    
-  ros::Subscriber sub = nh.subscribe("commands", 1000, &dataReceived);
-  ros::spin();
+  ros::Publisher blobs_pub = nh.advertise<std_msgs::String>("blobs", 1000);
+  // Nicola's code:
+  // pubPtr =
+  //     new ros::Publisher(nh.advertise<ebug_swarm_msg::frame>("blobs",1000)); 
+  // ros::Subscriber sub = nh.subscribe("commands", 1000, &dataReceived);
+  ros::Rate loop_rate(1);
+  int count = 0;
+  while (ros::ok()) {
+    std_msgs::String msg;
+    std::stringstream ss;
+
+    ss << "blobs blobs blobs" << count;
+    msg.data = ss.str();
+    ROS_INFO("%s", msg.data.c_str());
+    blobs_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+
+    count++;
+  } // while
   return 0;
-}
+} // main()
