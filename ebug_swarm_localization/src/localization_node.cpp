ec0d36fff9ad37089a6aa0b27f66f8fb06ef3582
Ahmet Sekercioglu <sekerci.ahmet@gmail.com>
2018-03-28 17:07:17 +0200

    Removed the control package.

--- ebug_swarm_localization/src/localization_node.cpp
+++ ebug_swarm_localization/src/localization_node.cpp
@@ -6,20 +6,20 @@
 
 ros::Publisher *pubPtr;
 
-void dataReceived(const ebug_swarm_msg::frame::ConstPtr& frameIn){
+void dataReceived(const ebug_swarm_msg::frame::ConstPtr& frameIn)
+{
   ROS_INFO_STREAM("Localization node: blobs arrived and poses sent.");
-
 }
 
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "localization_node");  
+  ros::init(argc, argv, "localization");  
   
   ros::NodeHandle nh;
-  pubPtr = new ros::Publisher(
-			      nh.advertise<ebug_swarm_msg::dataArray>("poses",1000));    
-  ros::Subscriber sub = nh.subscribe("blobs", 1000, &dataReceived);
+  //  pubPtr = new ros::Publisher(
+  //	      nh.advertise<ebug_swarm_msg::dataArray>("poses",1000));    
+  // ros::Subscriber sub = nh.subscribe("blobs", 1000, &dataReceived);
   ros::spin();
   return 0;
 }
